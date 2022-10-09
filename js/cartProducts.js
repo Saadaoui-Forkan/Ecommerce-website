5cdc9f1e5e1669eda0cce95fea3e41e5f6735371
Mahmoud <forkan.saadaoui.gmail.com>
2022-10-09 11:23:16 +0100

    remove products from cart

--- js/cartProducts.js
+++ js/cartProducts.js
@@ -1,12 +1,13 @@
-let productsInCart = JSON.parse(localStorage.getItem('productsInCart'))
-let productsInCartUI = document.querySelector('.cart-products')
-
-function drawProductsInCart() {
-    let cartProductsUI = productsInCart.map((product)=>{
+function init() {
+    let products = JSON.parse(localStorage.getItem("productsInCart")) || [];
+    let productsInCartUI = document.querySelector(".cart-products");
+    
+    // display products
+    productsInCartUI.innerHTML = products.map((product) => {
         return `
         <div class="product-container">
             <div class="product-img">
-                <img src='${product.img_url}.png' alt="" srcset="">
+            <img src='${product.img_url}.png' alt="" srcset="">
             </div>
             <div class="product-info">
                 <h2 class="product-title">${product.title}</h2>
@@ -14,11 +15,26 @@ function drawProductsInCart() {
                 <h2 class="product-price">${product.price} DT</h2>
             </div>
             <div class="product-btns">
-                <button class="remove-to-cart">Remove Product</button>
+                <button class="remove-to-cart" data-click="${product.id}">Remove Product</button>
             </div>
         </div>
-        `
-    })
-    productsInCartUI.innerHTML = cartProductsUI
+        `;
+    });
+
+    // remove products from cart
+    document.querySelectorAll(".remove-to-cart").forEach((item) => {
+        let id = item.getAttribute("data-click");
+        let productsInCart = localStorage.getItem("productsInCart");
+        if (productsInCart) {
+        item.addEventListener("click", () => {
+            let productsInCart = localStorage.getItem("productsInCart");
+            let items = JSON.parse(productsInCart);
+            let filtered = items.filter((data) => data.id != id);
+            localStorage.setItem("productsInCart", JSON.stringify(filtered));
+            init();
+        });
+        } 
+    });
 }
-drawProductsInCart()
+
+init();
